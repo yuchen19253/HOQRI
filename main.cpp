be19d535fef91edc14b39e68343fbcde73024d6f
yuchen19253 <yuchen_sunshine@163.com>
2022-09-18 22:45:41 -0400

    initial as ones

--- main.cpp
+++ main.cpp
@@ -25,12 +25,11 @@ int main () {
 //    int J[3] = {46952,46951,1592}; //facebook: 46952 x 46951 x 1592 with 738079 nonzeros
 //    int J[3] = {108035, 107253, 52955}; //Delicious: 108035 x 107253 x 52955 with 437593 nonzeros
 //    int J[3] = {2100, 18744, 12647}; //Last: 2100 x 18744 x 12647 with 186479 nonzeros
-//    int J[3] = {610,49961,8215}; //1M_MovieLen: 610, 49961, 8215 with 84159 nonzeros
-    int J[3] = {5,5,5};
+    int J[3] = {610,49961,8215}; //1M_MovieLen: 610, 49961, 8215 with 84159 nonzeros
+//    int J[3] = {4,4,4};
 
-//    int K[3] = {10,10,10};
-//    int J[3] = {3,3,4};
-    int K[3] = {2,2,2};
+    int K[3] = {10,10,10};
+//    int K[3] = {2,2,2};
     if(J[0]<K[0]||J[1]<K[1]||J[2]<K[2]){
         cout<<"Rank couldn't be large than original size!";
         return 0;
@@ -38,10 +37,12 @@ int main () {
     // read X from tensor file
     map<tuple<int,int,int>,double> mytensor;
     fstream tensorfile;
-    string filename = "/home/yuchen/Desktop/mytensor.txt";
-//    string filename = "/home/yuchen/Desktop/facebook.txt"; // /home/yuchen
-//    string filename = "/Users/yc/Desktop/MovieLen.txt";
+//    string filename = "/home/yuchen/Desktop/mytensor.txt";
+//    string filename = "/home/yuchen/Desktop/facebook.txt"; // /Users/yc
+    string filename = "/home/yuchen/Desktop/MovieLen.txt";
+//    string filename = "/home/yuchen/Desktop/Last.txt";
     tensorfile.open(filename,ios::in);
+
     if(tensorfile.is_open()){
         cout<<"Open file: "<<filename<<endl;
         string line;
@@ -101,6 +102,7 @@ int main () {
         }
     }
 
+
     /* allocate memory for the matrices Rs */
     double ** r1 = new double*[K[0]];
     double ** r2 = new double*[K[1]];
@@ -117,25 +119,28 @@ int main () {
     print_matrix("U", U, J[0], K[0]);
     print_matrix("V", V, J[1], K[1]);
     print_matrix("W", W, J[2], K[2]);
-
-    qr(U, J[0], K[0]);
-    qr(V, J[1], K[1]);
-    qr(W, J[2], K[2]);
-
-    print_matrix("U", U, J[0], K[0]);
-    print_matrix("V", V, J[1], K[1]);
-    print_matrix("W", W, J[2], K[2]);
+//
+//    qr(U, J[0], K[0]);
+//    qr(V, J[1], K[1]);
+//    qr(W, J[2], K[2]);
+//
+//    print_matrix("U", U, J[0], K[0]);
+//    print_matrix("V", V, J[1], K[1]);
+//    print_matrix("W", W, J[2], K[2]);
 //    print_matrix("r1", r1, K[0],K[0]);
 //    print_matrix("r2", r2, K[1],K[1]);
 //    print_matrix("r3", r3, K[2],K[2]);
 
     // update iteration
     int iteration = 50;
-    double newLoss(0), oldLoss(0), lossChange(0);
+    double *** G = ttm_update(mytensor, U, V, W, K);
+    double newLoss = norm_tensor(G,K), oldLoss(0), lossChange(0);
+
+    cout<<"Initial norm of G: " << newLoss <<endl;
     cout<<"Itr\t\tTime\t\tLoss Change\t\tnorm(core)"<<endl;
-    for(int itr=0; itr <= iteration; itr++){
-        clock_t start = clock();
-        oldLoss = newLoss;
+
+    clock_t start = clock();
+    for(int itr=1; itr <= iteration; itr++){
 //        cout<<"----------------------------------------- Iteration "<<itr+1<<" -----------------------------------------"<<endl;
 //        print_sptensor("X"+ to_string(itr), mytensor);
 
@@ -158,12 +163,11 @@ int main () {
 //        double *** G2 = sp2dense(ttmG,K);
 //        print_densetensor("G",G2,K);
 
-        double *** G = ttm_update(mytensor, U, V, W, K);
+//        double *** G = ttm_update(mytensor, U, V, W, K);
 //        print_densetensor("G",G,K);
 
 //        cout<< "calculate dense G "<< (double)(clock() - start) / (double)CLOCKS_PER_SEC << endl;
-        newLoss = norm_tensor(G,K);
-        lossChange = abs(newLoss - oldLoss);
+
 //        cout<< "calculate norm(G) "<< (double)(clock() - start) / (double)CLOCKS_PER_SEC << endl;
 
 
@@ -212,6 +216,13 @@ int main () {
         /* print the matrix Q resulting from gramSchmidt */
 //        print_matrix("newW", W, J[2],K[2]);
 
+        oldLoss = newLoss;
+        G = ttm_update(mytensor, U, V, W, K);
+        newLoss = norm_tensor(G,K);
+        lossChange = abs(newLoss - oldLoss);
+
+        double norm_G = norm_tensor(G, K);
+        std::cout << itr<< "\t\t" << (double)(clock() - start) / (double)CLOCKS_PER_SEC<< "\t\t" << lossChange<< "\t\t" << norm_G << endl;
 
 
         for(int i = 0; i < K[0]; i++) {
@@ -226,9 +237,6 @@ int main () {
         delete[] A1;
         delete[] A2;
         delete[] A3;
-
-        double norm_G = norm_tensor(G, K);
-        std::cout << itr<< "\t\t" << (double)(clock() - start) / (double)CLOCKS_PER_SEC<< "\t\t" << lossChange<< "\t\t" << norm_G << endl;
 
     }
 
